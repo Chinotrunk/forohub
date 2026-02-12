01ac06c69b5d38021a7963425cca9089d7b11e32
chinotrunk <gerson2050@hotmail.com>
2026-02-12 12:02:57 -0500

    fix(repository): correct CursoRepository generic type to use Curso entity

--- src/main/java/com/davidlima/forohub/repository/CursoRepository.java
+++ src/main/java/com/davidlima/forohub/repository/CursoRepository.java
@@ -1,7 +1,7 @@
 package com.davidlima.forohub.repository;
 
-import com.davidlima.forohub.domain.Usuario;
+import com.davidlima.forohub.domain.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CursoRepository extends JpaRepository<Usuario, Long> {
+public interface CursoRepository extends JpaRepository<Curso, Long> {
 }
