3fb57f217468ea8f14c8ef60b1af513815581c24
chinotrunk <gerson2050@hotmail.com>
2026-02-17 01:23:26 -0500

    feat(database): add default user insertion script for initial setup

---
+++ src/main/resources/db/migration/V2__insert_default_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO usuarios (nombre, correo_electronico, contrasena)
+VALUES (
+        'David',
+        'david@gmail.com',
+        '$2a$12$LkCxFyj2YlaxJVk1DJmgaeJ9i.bJAEVZuLuNEqsd.dOw4a6GgYb0O'
+    );
